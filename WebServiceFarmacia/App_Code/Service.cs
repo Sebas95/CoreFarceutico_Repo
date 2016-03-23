8031119fef70c3a4a49b1e26057ef20be57b9d3e
Sebas95 <sgonzalezquesada1@gmail.com>
2016-03-22 18:39:46 -0600

    Se agrego el ejemplo de una aplicación web
    Esta aplicacion esta dentro del mismo proyecto y consume el Web service

--- WebServiceFarmacia/App_Code/Service.cs
+++ WebServiceFarmacia/App_Code/Service.cs
@@ -1,54 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Web;
 using System.Web.Services;
 using System.Configuration;
 using Newtonsoft.Json;
 using System.Data.SqlClient;
-using WebServiceFarmacia;
 
-[WebService(Namespace = "http://tempuri.org/")]
-[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
-// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
- [System.Web.Script.Services.ScriptService]
 
-public class Service : System.Web.Services.WebService
+namespace WebServiceFarmacia
 {
-    public Service () {
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
 
-        //Uncomment the following line if using designed components 
-        //InitializeComponent(); 
-    }
-
-    [WebMethod]
-    public string HelloWorld() {
-        return "Hello World";
-    }
-
-    [WebMethod]
-    public void GetAllEmployees()
+    public class Service : System.Web.Services.WebService
     {
-        List<Employee> listEmployees = new List<Employee>();
+        public Service () {
 
-        string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
-        using (SqlConnection con = new SqlConnection(cs))
-        {
-            SqlCommand cmd = new SqlCommand("Select * from tblEmployees", con);
-            con.Open();
-            SqlDataReader rdr = cmd.ExecuteReader();
-            while (rdr.Read())
-            {
-                Employee employee = new Employee();
-                employee.id = Convert.ToInt32(rdr["Id"]);
-                employee.name = rdr["Name"].ToString();
-                employee.gender = rdr["Gender"].ToString();
-                employee.salary = Convert.ToInt32(rdr["Salary"]);
-                listEmployees.Add(employee);
-            }
+            //Uncomment the following line if using designed components 
+            //InitializeComponent(); 
         }
-        string json = JsonConvert.SerializeObject(listEmployees);
-        Context.Response.Write(json);
-    }
 
+        [WebMethod]
+        public string HelloWorld() {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public void GetAllEmployees()
+        {
+            List<Employee> listEmployees = new List<Employee>();
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from tblEmployees", con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Employee employee = new Employee();
+                    employee.id = Convert.ToInt32(rdr["Id"]);
+                    employee.name = rdr["Name"].ToString();
+                    employee.gender = rdr["Gender"].ToString();
+                    employee.salary = Convert.ToInt32(rdr["Salary"]);
+                    listEmployees.Add(employee);
+                }
+            }
+            string json = JsonConvert.SerializeObject(listEmployees);
+            Context.Response.Write(json);
+        }
+
+    }
 }
---
+++ WebServiceFarmacia/Scripts/Script.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="angular.min.js" />
+
+var app = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope, $http) {
+
+            $http.get("Service.asmx/GetAllEmployees")
+                .then(function (response) {
+                    $scope.employees = response.data;
+                 });
+        });

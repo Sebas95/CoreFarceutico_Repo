7c4178b63a5001aaef4d282d475a091748f032a7
Sebas95 <sgonzalezquesada1@gmail.com>
2016-03-25 10:24:59 -0600

    update

---
+++ FarmaTicaWebService/FarmaTicaWebService/Scripts/script.js
@@ -0,0 +1,25 @@
+﻿// Code goes here
+
+/*var app = angular
+        .module("myModule", ['ngResource'])
+        .controller("myController", function ($scope,$resource) {
+           $scope.list = [];
+           //var Resultado = $resource('https://httpbin.org/get',{},{get:{method:'GET'}});
+           var Resultado = $resource('https://httpbin.org/get',{}).query();
+           $scope.hello = "asdasdf";
+           alert(Resultado.data);
+           $scope.list = Resultado; 
+        });*/
+
+
+var app = angular
+        .module("myModule", ['ngResource'])
+        .factory('UserService', function ($resource) {
+            return $resource('http://localhost:8080/api/Client', {});
+        })
+        .controller("myController", function ($scope, $resource, UserService) {
+            alert("$scope.users");
+            $scope.users = UserService.query();
+            $scope.hello = "asdasdf";
+            alert($scope.users);
+        });

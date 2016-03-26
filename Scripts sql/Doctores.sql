1617154b3c4dfd202d2d55ebd24ab008bbe70119
JosafatV <josav09@gmail.com>
2016-03-26 17:56:24 -0600

    datos para insertar - doctores
    Agrego un script para insertar doctores

---
+++ Scripts sql/Doctores.sql
@@ -0,0 +1,32 @@
+USE FARMATICA;
+
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (10,'323453234', 'Alfred', 'Cen', '1889-01-02', 'Colorado');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (1, '323453815', 'Kregor', 'Than', '1815-01-02', 'Jericho Reach');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (2, '323453816', 'Khiron', 'Ruberec', '1820-01-02', 'Karybdis');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (3, '323453817', 'Meric', 'Voyen', '1785-01-02', 'Barbarus');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (4, '323453652', 'Fabius', 'Bile', '1525-01-02', 'Chemos');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (5, '323453818', 'Corbulo', 'Sanguinary', '1230-01-02', 'Baal');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (6, '323453830', 'Ulrik', 'Lupus', '1026-01-02', 'Fenris');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (7, '323453832', 'Sternhammer', 'Stormfang', '1702-01-02', 'Fenris');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (8, '323453833', 'Fabrikus', 'Farbarius', '1562-01-02', 'Terra');
+
+INSERT INTO DOCTOR (NoDoctor,Cedula,Nombre,Apellido,FechaNacimiento,Residencia) 
+	VALUES (9, '323453834', 'Garreon', 'Abnett', '1468-01-02', 'Badab Primaris');

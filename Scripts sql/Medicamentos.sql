fb609959608279dd705b7ca6556ba32a24d1a796
JosafatV <josav09@gmail.com>
2016-03-26 18:20:01 -0600

    datos para insertar - medicamentos
    script para insertar medicamentos

---
+++ Scripts sql/Medicamentos.sql
@@ -0,0 +1,17 @@
+USE FARMATICA;
+
+
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (111, 'Halothano', 1, 500, 'AbbottLabs');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (112, 'Ketamina', 1, 500, 'AbbottLabs');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (121, 'Bupivicaína', 0, 200, 'Pfizer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (122, 'Lidocaína', 0, 750, 'Bayer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (212, 'Ibuprofeno', 0, 500, 'Bayer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (213, 'Paracetamol', 0, 750, 'Bayer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (221, 'Codeína', 1, 1250, 'Pfizer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (222, 'Morfina', 1, 2150, 'Pfizer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (231, 'Alopurinol', 0, 500, 'AbbottLabs');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (311, 'Clorfenamina', 0, 300, 'Bayer');
+INSERT INTO MEDICAMENTO (Codigo,Nombre,Prescripcion,Costo,CasaFarmaceutica) VALUES (312, 'Dexametasona', 0, 200, 'Bayer');
+
+SELECT * FROM Medicamento;
+

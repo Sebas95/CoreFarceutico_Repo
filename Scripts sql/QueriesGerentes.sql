dcfc36ead0f86cc61610b78d4906779bdc4afeb5
JosafatV <josav09@gmail.com>
2016-03-27 18:04:32 -0600

    update
    se agregan queries para productos vendidos por el nuevo software

--- Scripts sql/QueriesGerentes.sql
+++ Scripts sql/QueriesGerentes.sql
@@ -10,16 +10,34 @@ SELECT COUNT(NoFactura) FROM PEDIDO WHERE (Estado='Completo' AND EMPRESA='P');
 
 SELECT COUNT(NoFactura) FROM PEDIDO WHERE (Estado='Completo' AND EMPRESA='F');
 
-/* Productos más vendidos por el nuevo software*/
+/* Productos más vendidos */
 SELECT SUM(Mpp.Cantidad), M.Nombre
 FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
-WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo';
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND ;
+
+/* Productos más vendidos por compañia */
+SELECT SUM(Mpp.Cantidad), M.Nombre
+FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa='P';
+
+SELECT SUM(Mpp.Cantidad), M.Nombre
+FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa='F';
+
+/**
+/* Productos más vendidos por el nuevo software */
+SELECT SUM(Mpp.Cantidad), M.Nombre
+FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Orden='W'; /*Orden Web*/
 
 /* Productos más vendidos por el nuevo software por compañia*/
 SELECT SUM(Mpp.Cantidad), M.Nombre
 FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
-WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa = 'P';
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa='P' AND P.Orden='W'; /*Orden Web*/
 
 SELECT SUM(Mpp.Cantidad), M.Nombre
 FROM MEDICAMENTOS_POR_PEDIDO AS Mpp, PEDIDO AS P, MEDICAMENTO AS M
-WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa = 'F';
+WHERE P.NoFactura=Mpp.NoFactura AND Mpp.CodigoMedicamento=M.Codigo AND P.Estado='Completo' AND P.Empresa='F' AND P.Orden='W'; /*Orden Web*/
+
+**/
+

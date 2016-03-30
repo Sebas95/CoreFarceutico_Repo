e329e4a8965547fe2e17ca378ae84d5a5f10bb56
JosafatV <josav09@gmail.com>
2016-03-29 21:31:21 -0600

    Actualización addPedidos
    Se agrega el formulario para hacer pedidos

--- Scripts sql/EmpleadosCRUD.sql
+++ Scripts sql/EmpleadosCRUD.sql
@@ -18,3 +18,18 @@ WHERE IdEmpleado= '' ;
 DELETE FROM EMPLEADO
 WHERE IdEmpleado= '' ;
 
+
+/*
+	Malcador Sigillite, G, F
+	Horus Lupercal, G, P
+
+		Lion ElJonson, D
+		Jaghatai Khan, D
+		Leman Russ, D
+		Rogal Dorn, D
+		Ferrus Manus, D
+		Roboute Gulliman, D
+		Corvus Corax, D
+		Alpharius Omegon, D
+		Konrad Curze, D
+		*/

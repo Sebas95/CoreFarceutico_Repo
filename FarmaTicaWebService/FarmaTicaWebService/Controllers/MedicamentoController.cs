f740f20cb6a441f54f40d026a0128695c1ea3ff2
Sebas95 <sgonzalezquesada1@gmail.com>
2016-04-04 16:03:52 -0600

    Medicmanetos por sucursal

--- FarmaTicaWebService/FarmaTicaWebService/Controllers/MedicamentoController.cs
+++ FarmaTicaWebService/FarmaTicaWebService/Controllers/MedicamentoController.cs
@@ -11,31 +11,67 @@ namespace FarmaTicaWebService.Controllers
 {
     public class MedicamentoController : ApiController
     {
-        public MedicamentosAccess databaseAccess = new MedicamentosAccess();
+        /// <summary>
+        /// An object that has access to the Medicmento Table by Sql connection
+        /// </summary>
+        private MedicamentosAccess databaseAccess = new MedicamentosAccess();
 
-        /** Retorna toda la lista de medicamentos
-        */
+
+        /// <summary>
+        /// Gets all the medicaments of the Table Medicamento
+        /// </summary>
+        /// <returns> List<Medicamento> </returns>
         public List<Medicamento> Get()
         {
             return databaseAccess.getAllMedicamentos();
         }
+        /// <summary>
+        /// Gets an specific Medicamento object identified by the id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> Medicamento </returns>
         public Medicamento Get(string id)
         {
             return databaseAccess.getMedicamento(id);
         }
+        [Route("api/Medicamento/{Prescripcion}/{NoSucursal}")]
+        public List<Medicamento> Get(string Prescripcion, string NoSucursal)
+        {
+            return databaseAccess.getAllMedicamentosPorSucursal(Prescripcion, NoSucursal);
+        }
+        /// <summary>
+        /// Returns all the Medicamentos filtered by the attribute Prescripcion
+        /// </summary>
+        /// <param name="Prescripcion"></param>
+        /// <returns>  List<Medicamento>  </returns>
         [Route("api/Medicamento/Prescripcion/{Prescripcion}")]
         public List<Medicamento> GetMedicamentosPrescripcion(string Prescripcion)
         {
             return databaseAccess.getAllMedicamentos(Prescripcion);
         }
+        /// <summary>
+        /// Posts a new Medicamento object
+        /// </summary>
+        /// <param name="medicamento"></param>
+        /// <returns>The new  Medicamento </returns>
         public Medicamento Post(Medicamento medicamento)
         {
             return databaseAccess.addMedicamento(medicamento);
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="medicamento"></param>
+        /// <returns></returns>
         public Medicamento Put(string id, [FromBody] Medicamento medicamento)
         {
             return databaseAccess.updateMedicamento(id,medicamento);
         }
+        /// <summary>
+        /// Deletes a Medicamento identified by id
+        /// </summary>
+        /// <param name="id"></param>
         public void Delete(string id)
         {
             databaseAccess.deleteMedicamento(id);
--- FarmaTicaWebService/FarmaTicaWebService/DataBase/MedicamentosAccess.cs
+++ FarmaTicaWebService/FarmaTicaWebService/DataBase/MedicamentosAccess.cs
@@ -10,6 +10,10 @@ namespace FarmaTicaWebService.DataBase
 {
     public class MedicamentosAccess
     {
+        /// <summary>
+        /// Selects all the rows of Meidcamento table, maps to Medicamento objects and returns it
+        /// </summary>
+        /// <returns> List<Medicamento> </returns>
         public List<Medicamento> getAllMedicamentos()
         {
             List < Medicamento > listMedicamentos  = new List<Medicamento>();
@@ -36,6 +40,11 @@ namespace FarmaTicaWebService.DataBase
             }
             return listMedicamentos;
         }
+        /// <summary>
+        /// Selects all the rows of Meidcamento table filtered by Prescripcion attribute, maps to Medicamento objects and returns it
+        /// </summary>
+        /// <param name="Prescripcion"> Tha attribute Prescripcion (0 or 1) </param>
+        /// <returns> List<Medicamento></returns>
         public List<Medicamento> getAllMedicamentos(string Prescripcion)
         {
             List<Medicamento> listMedicamentos = new List<Medicamento>();
@@ -62,6 +71,39 @@ namespace FarmaTicaWebService.DataBase
             }
             return listMedicamentos;
         }
+        public List<Medicamento> getAllMedicamentosPorSucursal(string Prescripcion,string Nosucursal)
+        {
+            List<Medicamento> listMedicamentos = new List<Medicamento>();
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand(
+                    "SELECT M.Nombre, M.Codigo , M.Prescripcion , M.CasaFarmaceutica , M.Costo  FROM MEDICAMENTO AS M JOIN MEDICAMENTO_EN_SUCURSAL AS MS" 
+                    +" ON M.Codigo = MS.CodigoMedicamento " 
+                    +" WHERE M.Prescripcion = '" + Prescripcion + "'  AND MS.NoSucursal = '"+ Nosucursal +"' AND MS.Cantidad > 0  ; "
+                    , con);
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read()) //si existe en la base de datos
+                {
+                    Medicamento medicamento = new Medicamento();
+                    medicamento.Nombre = rdr["Nombre"].ToString();
+                    medicamento.codigo = rdr["Codigo"].ToString();
+                    medicamento.Prescripcion = rdr["Prescripcion"].ToString();
+                    if (medicamento.Prescripcion == "True") { medicamento.Prescripcion = "1"; }
+                    if (medicamento.Prescripcion == "False") { medicamento.Prescripcion = "0"; }
+                    medicamento.CasaFarmaceutica = rdr["CasaFarmaceutica"].ToString();
+                    medicamento.Costo = rdr["Costo"].ToString();
+                    listMedicamentos.Add(medicamento);
+                }
+            }
+            return listMedicamentos;
+        }
+        /// <summary>
+        /// Selects a specific row of Medicamento table and maps into an object Medicamento 
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <returns></returns>
         public Medicamento getMedicamento(string codigo)
         {
             Medicamento medicamento = new Medicamento();
@@ -89,6 +131,11 @@ namespace FarmaTicaWebService.DataBase
             }
             return medicamento;
         }
+        /// <summary>
+        /// INserts a row in the table Medicamento
+        /// </summary>
+        /// <param name="medicamento"> The object tobe mapped and inserted </param>
+        /// <returns> The new object Medicamento inserted </returns>
         public Medicamento addMedicamento(Medicamento medicamento)
         {
             string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
@@ -104,7 +151,12 @@ namespace FarmaTicaWebService.DataBase
             }
             return medicamento;
         }
-
+        /// <summary>
+        /// Updates an specific Medicamento row
+        /// </summary>
+        /// <param name="Codigo"> The id of the row or primary key </param>
+        /// <param name="medicamento"> The medicamento object modified fr updating </param>
+        /// <returns> The Medicamento object inserted </returns>
         public Medicamento updateMedicamento(string Codigo, Medicamento medicamento)
         {
             string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
@@ -120,7 +172,10 @@ namespace FarmaTicaWebService.DataBase
             }
             return medicamento;
         }
-
+        /// <summary>
+        /// Deletes a row i the tabe Medicamento
+        /// </summary>
+        /// <param name="Codigo"> The id of the Medicamento object to be deleted </param>
         public void deleteMedicamento(string Codigo)
         {
             string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
